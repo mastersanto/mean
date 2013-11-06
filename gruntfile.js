f4b406a316b82d479cf106bd0f75a4c867bef6bb
Jorge Sandoval <jorgeivansandoval@gmail.com>
2013-11-05 16:26:39 -0800

    MongoDB configuration

--- gruntfile.js
+++ gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function(grunt) {
         },
         env: {
             test: {
-                NODE_ENV: 'production'
+                NODE_ENV: 'test'
             }
         }
     });
